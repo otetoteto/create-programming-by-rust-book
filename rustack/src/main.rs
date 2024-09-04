1854850b85dbb8aa38c6e9e87fcf5af46d011e39
otetoteto <oteto2oteto@gmail.com>
2024-09-05 00:15:49 +0900

    2.4

--- rustack/src/main.rs
+++ rustack/src/main.rs
@@ -1,14 +1,49 @@
 fn main() {
     for line in std::io::stdin().lines() {
+        let mut stack = vec![];
+
         if let Ok(line) = line {
             let words: Vec<_> = line.split(" ").collect();
-            println!("Line: {words:?}");
+
+            for word in words {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
+                        _ => panic!("{word:?} could not be parsed"),
+                    }
+                }
+            }
         }
+
+        println!("stack: {stack:?}");
     }
 }
 
 fn add(stack: &mut Vec<i32>) {
-    let l = stack.pop().unwrap();
     let r = stack.pop().unwrap();
+    let l = stack.pop().unwrap();
     stack.push(l + r);
+}
+
+fn sub(stack: &mut Vec<i32>) {
+    let r = stack.pop().unwrap();
+    let l = stack.pop().unwrap();
+    stack.push(l - r);
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let r = stack.pop().unwrap();
+    let l = stack.pop().unwrap();
+    stack.push(l * r);
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let r = stack.pop().unwrap();
+    let l = stack.pop().unwrap();
+    stack.push(l / r);
 }
