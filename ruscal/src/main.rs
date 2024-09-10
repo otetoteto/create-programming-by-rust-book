09b90f019ccd3be05cc4c39eba83fb07042a2385
otetoteto <oteto2oteto@gmail.com>
2024-09-10 23:28:00 +0900

    3.2

---
+++ ruscal/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn whitespace(mut input: &str) -> &str {
+    while matches!(input.chars().next(), Some(' ')) {
+        let mut chars = input.chars();
+        chars.next();
+        input = chars.as_str();
+    }
+    input
+}
+
+fn number(mut input: &str) -> &str {
+    if matches!(input.chars().next(), Some(_x @ ('-' | '+' | '0'..='9'))) {
+        let mut chars = input.chars();
+        chars.next();
+        input = chars.as_str();
+        while matches!(input.chars().next(), Some(_x @ ('.' | '0'..='9'))) {
+            let mut chars = input.chars();
+            chars.next();
+            input = chars.as_str();
+        }
+    }
+    input
+}
+
+fn ident(mut input: &str) -> &str {
+    if matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
+        while matches!(
+            input.chars().next(),
+            Some(_x @ ('a'..='z' | 'A'..='Z' | '0'..='9'))
+        ) {
+            let mut chars = input.chars();
+            chars.next();
+            input = chars.as_str();
+        }
+    }
+    input
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_whitespace() {
+        assert_eq!(whitespace("    "), "");
+    }
+
+    #[test]
+    fn test_number() {
+        assert_eq!(number("-123.4 "), " ");
+    }
+
+    #[test]
+    fn test_ident() {
+        assert_eq!(ident("hoge5 123"), " 123");
+    }
+}
